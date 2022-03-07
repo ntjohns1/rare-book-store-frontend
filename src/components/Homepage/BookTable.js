315f93ad77d3a4c8df5c2193375a0dd31efcb38b
ntjohns1 <nelsontjohns@gmail.com>
2022-03-07 02:05:32 -0500

    styling on book table element

--- src/components/Homepage/BookTable.js
+++ src/components/Homepage/BookTable.js
@@ -1,42 +1,44 @@
-import { Table } from "react-bootstrap"
+import { Card, Table } from "react-bootstrap"
 import books from "../../util/bookSeeds"
 export default function BookTable() {
     function goToBook(bookId) {
         document.location.replace(`/book/${bookId}`);
     }
     return (
-        <Table striped bordered hover variant="dark">
+            <Card bg="dark">
+                <Card.Title className="text-center pt-2 text-white"><h3>Book Inventory</h3></Card.Title>
+            <Table striped bordered hover variant="dark">
 
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Genre</th>
-                    <th>Year Published</th>
-                    <th>Edition</th>
-                    <th>Biding</th>
-                    <th>Condition</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {books.map((book) => (
-                    // <tr onClick={() => goToSingleBook(book._id)}
-                    <tr onClick={() => goToBook(book.id)} key={book.id}>
-                        <td>{book.id}</td>
-                        <td>{book.title}</td>
-                        <td>{book.author}</td>
-                        <td>{book.genre}</td>
-                        <td>{book.yearPublished}</td>
-                        <td>{book.edition}</td>
-                        <td>{book.binding}</td>
-                        <td>{book.condition}</td>
-                        <td>${book.price}</td>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Genre</th>
+                        <th>Year Published</th>
+                        <th>Edition</th>
+                        <th>Biding</th>
+                        <th>Condition</th>
+                        <th>Price</th>
                     </tr>
-                ))}
-            </tbody>
-        </Table>
-
+                </thead>
+                <tbody>
+                    {books.map((book) => (
+                        // <tr onClick={() => goToSingleBook(book._id)}
+                        <tr onClick={() => goToBook(book.id)} key={book.id}>
+                            <td>{book.id}</td>
+                            <td>{book.title}</td>
+                            <td>{book.author}</td>
+                            <td>{book.genre}</td>
+                            <td>{book.yearPublished}</td>
+                            <td>{book.edition}</td>
+                            <td>{book.binding}</td>
+                            <td>{book.condition}</td>
+                            <td>${book.price}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+            </Card>
     )
 }
